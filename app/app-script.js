6847ebfc0a14455cc04989fcd7409f41d224ba14
thegr33d <117223968+thegr33d@users.noreply.github.com>
2024-06-06 12:20:38 +0200

    Furthers Law Disclaimers

--- app/app-script.js
+++ app/app-script.js
@@ -1,3 +1,24 @@
+/*!
+ * Paths Finder
+ * 
+ * Copyright (c) 2024 Paths Finder - Agnello Renato Nicolae
+ * 
+ * Licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0) License.
+ * You may obtain a copy of the License at https://creativecommons.org/licenses/by-nc-nd/4.0/
+ * 
+ * You are free to:
+ * - Share — copy and redistribute the material in any medium or format
+ * 
+ * Under the following terms:
+ * - Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+ * - NonCommercial — You may not use the material for commercial purposes.
+ * - NoDerivatives — If you remix, transform, or build upon the material, you may not distribute the modified material.
+ * - No additional restrictions — You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+ * 
+ * This project uses the vis.js library, which is licensed under the MIT License.
+ */
+
+
 // variabile per visualizzare i messaggi utili al debug e alla comprensione del codice
 let debug = true;
 
